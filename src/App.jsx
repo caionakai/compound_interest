a5d58ef5db3228c8f0476133a7422f91ea78dbf4
Caio Nakai <caio.nakai@nokia.com>
2023-03-15 20:53:48 +0000

    add compound interest definition text

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,17 @@ function App() {
           <ResultBox title="Juros" value={results.interest} />
         </div>
       </div>
+      <div className={styles.body}>
+        <b>O que são juros compostos?</b>
+        <p>
+          Juros compostos são a adição de juros ao valor principal de um
+          empréstimo ou depósito, ou seja, juros sobre o valor principal mais
+          juros. É o resultado de reinvestir os juros, ou adicioná-los ao
+          capital emprestado em vez de pagá-los, ou exigir o pagamento do
+          credor, de modo que os juros no próximo período sejam ganhos sobre a
+          soma principal mais os juros acumulados anteriormente.
+        </p>
+      </div>
     </div>
   );
 }
